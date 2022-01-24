348c9d9234e4db9b75b4bee74eb302fbff6ccc47
Francisco Possamai <franpossa1@gmail.com>
2022-01-24 07:59:00 -0300

    Funcionalidad Js completa

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
+let buscadorDeTexto = document.querySelector(".buscatex");
+
 let ClimaAPI = {
-  
-    apiKey: "d88a3271bc7a85c4292f767e82a45dc7",
+  apiKey: "d88a3271bc7a85c4292f767e82a45dc7",
   fetchWeather: function (ciudad) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q="+ ciudad +"&units=metric" +this.apiKey//+"&lang=sp",
-    ).then((respuesta)=>respuesta.json())
-    .then((data)=>this.mostrarClima(data));
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        ciudad +
+        "&appid=" +
+        this.apiKey + "&lang=sp"
+    )
+      .then((respuesta) => respuesta.json())
+      .then((data) => this.mostrarClima(data));
   },
-  mostrarClima : function (data){
-      const {name} = data;
-      const {icon,description} = data.weather[0];
-      const {temp, humidity}= data.main;
-      const {speed} =data.wind; 
-      console.log(data)
-      console.log(`ARROJANDO RESULTADOS :  ${name}`);
+  mostrarClima: function (data) {
+    
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(data);
+    console.log(`ARROJANDO RESULTADOS :  ${name}`);
     document.querySelector(".clima").innerText = `Clima en ${name}`;
-    document.querySelector(".temperatura-clima").innerText = ` ${(temp)} °C`;
+    document.querySelector(".temperatura-clima").innerText = ` ${Math.round((temp)-273)} °C`;
     document.querySelector(".descripcion").innerText = `${description}`;
-    document.querySelector(".humedad").innerText = `Humedad: ${humidity}`;
+    document.querySelector(".humedad").innerText = `Humedad: ${humidity} %`;
     document.querySelector(".viento").innerText = `Viento: ${speed} Km/h`;
-    document.querySelector(".icono-clima").src = `https://openweathermap.org/img/wn/${icon}@2x.png`; 
+    document.querySelector(
+      ".icono-clima"
+    ).src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    buscadorDeTexto.value="";
     
-    
-    }
-
+  },
+  buscar :function (){
+    this.fetchWeather(buscadorDeTexto.value)
   }
-
-
+};
+document.querySelector(".accion-buscar").addEventListener("click",function(event){
+  event.preventDefault();
+  ClimaAPI.buscar();
+})
